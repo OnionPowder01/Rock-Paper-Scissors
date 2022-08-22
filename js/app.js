754dd6e81f3041a6c04c795a75bc08f60b9d6084
OnionPowder01 <autonatione36@gmail.com>
2022-08-22 21:37:23 +0300

    Edit HTML

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-//  Create variables for the score 
-let playerScore = 0;
-let computerScore = 0;
+const rock = document.getElementById("rockBtn");
+const paper = document.getElementById("paperBtn");
+const scissors = document.getElementById("scissorsBtn");
+
 
 // Get the random computer choice between Rock, Paper, Scissors
 function getComputerChoice() {
@@ -50,29 +51,52 @@ function playRound(playerSelection, computerSelection) {
 
 
 function gameCompleted() {
- for (let i = 0; i <= 5; i++){
-    if (i < 5) {
+ 
         // Run the computer choice, playerselection(prompt), playround and show the score in realtime
         const computerSelection = getComputerChoice();
-        const playerSelection = prompt('Rock, Paper, Scissors');
+        // const playerSelection = prompt('Rock, Paper, Scissors');
         let game = playRound(playerSelection, computerSelection);
         console.log(game);
         console.log(`player score is ${playerScore}`);
         console.log(`Computer score is ${computerScore}`);
-         } 
+         
          //When i gets to 5 run the else to decide the winner
-    else {
-        if (playerScore > computerScore) {
-            console.log('The Humanity has won')
-        } else {
-            console.log('Terminator is coming')
+   
+       
         }
-    }  
-       }
-    }
     
-gameCompleted();
+    
+    
+let playerScore = 0;
+let computerScore = 0;  
+
+const btns = document.querySelectorAll('.button');
+btns.forEach(button => button.addEventListener('click', playRound));
+btns.addEventListener('click', gameCompleted);
 
 
 
 
+
+/* function gameCompleted() {
+    for (let i = 0; i <= 5; i++){
+       if (i < 5) {
+           // Run the computer choice, playerselection(prompt), playround and show the score in realtime
+           const computerSelection = getComputerChoice();
+           const playerSelection = prompt('Rock, Paper, Scissors');
+           let game = playRound(playerSelection, computerSelection);
+           console.log(game);
+           console.log(`player score is ${playerScore}`);
+           console.log(`Computer score is ${computerScore}`);
+            } 
+            //When i gets to 5 run the else to decide the winner
+       else {
+           if (playerScore > computerScore) {
+               console.log('The Humanity has won')
+           } else {
+               console.log('Terminator is coming')
+           }
+       }  
+          }
+       }
+    */
